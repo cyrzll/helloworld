a9d9d60887313833cca4131637412ed6da9c3385
cyrzll <rizalajipermana@gmail.com>
2026-04-19 22:05:40 +0700

    first commit

---
+++ src/components/page/HelloWorld.jsx
@@ -0,0 +1,7 @@
+export default function HelloWorldPage() {  
+    const thisText = "Hello World!";
+
+    return (
+        <h1 className="font-serif text-2xl md:text-4xl xl:text-5xl">{thisText}</h1>
+    )
+}
